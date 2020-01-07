b6fe199a1389ca164df3fe8d9309b156251252a1
Viktor Borsodi <borsodiviktor@gmail.com>
2020-01-07 12:59:47 +0100

    Updates

---
+++ precache-manifest.e4dfd5abb6680b3b96ca2ad760c6293f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b90851872145056c1ffa490d830df312",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "2a6301e666dbbe51fcfb",
+    "url": "/memory-game/static/css/main.359eeb13.chunk.css"
+  },
+  {
+    "revision": "adc971196a91fdcefebe",
+    "url": "/memory-game/static/js/2.375305e2.chunk.js"
+  },
+  {
+    "revision": "67935b6d863b3e23bc08cf31dbc40ecc",
+    "url": "/memory-game/static/js/2.375305e2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2a6301e666dbbe51fcfb",
+    "url": "/memory-game/static/js/main.05792d4b.chunk.js"
+  },
+  {
+    "revision": "fb79d6b73fef31faac88",
+    "url": "/memory-game/static/js/runtime-main.9906d813.js"
+  }
+]);
